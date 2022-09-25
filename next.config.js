3bcd7a30c238dc2170ea46f93edbc0574561e7b5
Pedro Sobral <pedrosobral666.uk@gmail.com>
2022-09-25 15:53:41 +0100

    Add project configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')(['three']);
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	withTM,
+};
+
+module.exports = nextConfig;
